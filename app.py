6980379b1b4fa3ae66ae86dfb0be39734bad412e
Crystal Oakes <Neocat333@users.noreply.github.com>
2023-03-30 17:14:57 +0000

    Added html and CSS

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 # see https://flask.palletsprojects.com/en/2.2.x/quickstart/
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return 'hello'
+	return render_template('home.html')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
